ef614e70db02cd7d5b5f61ea1d25bf4e68a4d9a5
Anton Vakulenko <antonvakulenko@Antons-MacBook-Pro.local>
2025-09-20 10:49:39 +0300

    feat(web): create lands case and scratch

---
+++ src/views/FrScratchView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fr-scratch-view">
+    <FrScratchLanding />
+  </div>
+</template>
+
+<script setup lang="ts">
+import FrScratchLanding from '@/components/FrScratchLanding.vue'
+</script>
+
+<style scoped>
+.fr-scratch-view {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
+
+
